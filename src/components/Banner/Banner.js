e028d97ff3fcfcf0ed881182a278b458290878f1
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-05-08 11:44:30 +0600

    banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -14,33 +14,7 @@ const Banner = () => {
             <div className='d-flex justify-content-center align-items-center container'>
 
 
-                <div className='d-flex justify-content-center row align-items-center container'>
-                    <div className='col-lg-6'>
-                        <small>MOTORBIKE</small>
-
-                        <div>
-                            <div>
-                                <div>
-                                    <h1>HUNT YOUR BIKE</h1>
-                                    <p>Safe Trading E Bike on Leading B2B Platform. Quality E Bike with Competitive Price</p>
-                                </div>
-                            </div>
-
-                        </div>
-
-
-
-                    </div>
-                    <div className='d-flex justify-content-end col-lg-6 '>
-                        <div>
-
-
-                            <div ><img className=' banner-img' src={bannerImg} alt="" /></div>
-
-                        </div>
-
-                    </div>
-                </div>
+               
 
 
 
